c3f1c3a7cfcee00e767a5da13032b896b82a137f
smitha110 <smitha110@mailbox.winthrop.edu>
2016-02-17 16:15:57 +0000

    IP address example using geoip website.

---
+++ GeoIP/GeoIP.php
@@ -0,0 +1,24 @@
+<?php
+
+class GeoIP
+{
+    protected  $fp = "";
+    protected $ip = "";
+    protected $state = "";
+    protected $city = "";
+    
+    public function __construct()
+    {
+        
+    }
+    
+    public function getIP()
+    {
+        
+    }
+    
+    public function getState()
+    {
+        
+    }
+}
