f083fe81a870e72e7db9f26cd44eafa8e4a04930
smitha110 <smitha110@mailbox.winthrop.edu>
2016-02-18 15:55:30 +0000

    Code for question 2.

--- q2/SimpleAccount.php
+++ q2/SimpleAccount.php
@@ -4,7 +4,27 @@ class SimpleAccount
 {
 
 	/* BEGIN ANSWER SECTION */
-
+	public $balance = 0.00;
+	
+	public function __construct()	//Constructor for SimpleAccount
+	{
+		
+	}
+	
+	public function deposit($amount)	//Add the amount to the balance
+	{
+		$this->balance = $balance + $amount;
+	}
+	
+	public function withdrawal($amount)	//Subtract the amount from the balance
+	{
+		$this->balance = $amount - $balance;
+	}
+	
+	public function getBalance()	//Return the balance of the account
+	{
+		return $this->balance;
+	}
 	/* END ANSWER SECTION */
 
 }
